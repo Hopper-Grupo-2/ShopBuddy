36ee5b42251f1f0af6c7f0f880c64885d2e26fb8
Felipe Fernandes <felipe.bravil@gmail.com>
2023-07-15 16:46:20 -0300

    adjust enviroment variables

--- server/src/database/mongodb.ts
+++ server/src/database/mongodb.ts
@@ -3,10 +3,12 @@ import mongoose from "mongoose";
 const MONGO_USER = process.env.MONGO_USER;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 const MONGO_DB = process.env.MONGO_DB;
-const mongoURI = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@mongo:27017/${MONGO_DB}`;
+const NODE_ENV = process.env.NODE_ENV;
+const mongoURI = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@mongo${NODE_ENV}:27017/${MONGO_DB}`;
 
 export default async function connectDB() {
 	try {
+		console.log(mongoURI);
 		await mongoose.connect(mongoURI);
 	} catch (err) {
 		console.log(err);
