971f8229bafe436faf2883b6cb0db9382a59d80e
Letonio <letoniojs@gmail.com>
2023-07-18 20:35:08 -0300

    add patch and delete users

--- server/src/interfaces/user.ts
+++ server/src/interfaces/user.ts
@@ -1,12 +1,24 @@
 import { Schema } from "mongoose";
 
 export default interface IUser {
-	_id?: Schema.Types.ObjectId | string;
-	username: string;
-	email: string;
-	password: string;
-	firstName: string;
-	lastName: string;
-	createdAt?: Date;
-	updatedAt?: Date;
+    _id?: Schema.Types.ObjectId | string;
+    username: string;
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+export interface IUserUpdate {
+    _id?: Schema.Types.ObjectId | string;
+    username: string;
+    email: string;
+    oldPassword: string;
+    newPassword: string;
+    firstName: string;
+    lastName: string;
+    createdAt?: Date;
+    updatedAt?: Date;
 }
--- server/src/repositories/users.ts
+++ server/src/repositories/users.ts
@@ -3,151 +3,222 @@ import ErrorHandler from "../errors";
 import IUser from "../interfaces/user";
 
 export default class UsersRepositories {
-	private static Model = Models.getInstance().userModel;
+    private static Model = Models.getInstance().userModel;
 
-	public static async getUserById(userId: string): Promise<IUser | null> {
-		try {
-			const response = await this.Model.findOne({ _id: userId });
+    public static async getUserById(userId: string): Promise<IUser | null> {
+        try {
+            const response = await this.Model.findOne({ _id: userId });
 
-			if (response === null) return null;
+            if (response === null) return null;
 
-			const user: IUser = {
-				_id: response._id,
-				username: response.username,
-				email: response.email,
-				password: response.password,
-				firstName: response.firstName,
-				lastName: response.lastName,
-				createdAt: response.createdAt,
-				updatedAt: response.updatedAt,
-			};
-			return user;
-		} catch (error) {
-			console.error(this.name, "getUserById error: ", error);
-			throw ErrorHandler.createError(
-				"InternalServerError",
-				"Internal server error"
-			);
-		}
-	}
+            const user: IUser = {
+                _id: response._id,
+                username: response.username,
+                email: response.email,
+                password: response.password,
+                firstName: response.firstName,
+                lastName: response.lastName,
+                createdAt: response.createdAt,
+                updatedAt: response.updatedAt,
+            };
+            return user;
+        } catch (error) {
+            console.error(this.name, "getUserById error: ", error);
+            throw ErrorHandler.createError(
+                "InternalServerError",
+                "Internal server error"
+            );
+        }
+    }
 
-	public static async getUserByEmail(email: string): Promise<IUser | null> {
-		try {
-			const response = await this.Model.findOne({ email });
+    public static async getUserByEmail(email: string): Promise<IUser | null> {
+        try {
+            const response = await this.Model.findOne({ email });
 
-			if (response === null) return null;
+            if (response === null) return null;
 
-			const user: IUser = {
-				_id: response._id,
-				username: response.username,
-				email: response.email,
-				password: response.password,
-				firstName: response.firstName,
-				lastName: response.lastName,
-				createdAt: response.createdAt,
-				updatedAt: response.updatedAt,
-			};
-			return user;
-		} catch (error) {
-			console.error(this.name, "getUserByEmail error: ", error);
-			throw ErrorHandler.createError(
-				"InternalServerError",
-				"Internal server error"
-			);
-		}
-	}
+            const user: IUser = {
+                _id: response._id,
+                username: response.username,
+                email: response.email,
+                password: response.password,
+                firstName: response.firstName,
+                lastName: response.lastName,
+                createdAt: response.createdAt,
+                updatedAt: response.updatedAt,
+            };
+            return user;
+        } catch (error) {
+            console.error(this.name, "getUserByEmail error: ", error);
+            throw ErrorHandler.createError(
+                "InternalServerError",
+                "Internal server error"
+            );
+        }
+    }
 
-	public static async getUserByUsername(
-		username: string
-	): Promise<IUser | null> {
-		try {
-			const response = await this.Model.findOne({ username });
+    public static async getUserByUsername(
+        username: string
+    ): Promise<IUser | null> {
+        try {
+            const response = await this.Model.findOne({ username });
 
-			if (response === null) return null;
+            if (response === null) return null;
 
-			const user: IUser = {
-				_id: response._id,
-				username: response.username,
-				email: response.email,
-				password: response.password,
-				firstName: response.firstName,
-				lastName: response.lastName,
-				createdAt: response.createdAt,
-				updatedAt: response.updatedAt,
-			};
-			return user;
-		} catch (error) {
-			console.error(this.name, "getUserByUsername error: ", error);
-			throw ErrorHandler.createError(
-				"InternalServerError",
-				"Internal server error"
-			);
-		}
-	}
+            const user: IUser = {
+                _id: response._id,
+                username: response.username,
+                email: response.email,
+                password: response.password,
+                firstName: response.firstName,
+                lastName: response.lastName,
+                createdAt: response.createdAt,
+                updatedAt: response.updatedAt,
+            };
+            return user;
+        } catch (error) {
+            console.error(this.name, "getUserByUsername error: ", error);
+            throw ErrorHandler.createError(
+                "InternalServerError",
+                "Internal server error"
+            );
+        }
+    }
 
-	public static async createNewUser(user: IUser): Promise<IUser> {
-		try {
-			const response = await this.Model.create({
-				username: user.username,
-				email: user.email,
-				password: user.password,
-				firstName: user.firstName,
-				lastName: user.lastName,
-				createdAt: Date.now(),
-				updatedAt: Date.now(),
-			});
-			const createdUser: IUser = {
-				_id: response._id,
-				username: response.username,
-				email: response.email,
-				password: response.password,
-				firstName: response.firstName,
-				lastName: response.lastName,
-				createdAt: response.createdAt,
-				updatedAt: response.updatedAt,
-			};
-			return createdUser;
-		} catch (error) {
-			console.error(this.name, "createNewUser error: ", error);
-			throw ErrorHandler.createError(
-				"InternalServerError",
-				"Internal server error"
-			);
-		}
-	}
-	public static async getAllUsers() {
-		try {
-			const response = await this.Model.find();
-			const allUsers: IUser[] = [];
-			response.forEach((user) => {
-				allUsers.push({
-					_id: user._id,
-					username: user.username,
-					email: user.email,
-					password: user.password,
-					firstName: user.firstName,
-					lastName: user.lastName,
-					createdAt: user.createdAt,
-					updatedAt: user.updatedAt,
-				});
-			});
-			return allUsers;
-		} catch (error) {
-			console.error(this.name, "getAllUsers error", error);
-			throw ErrorHandler.createError(
-				"InternalServerError",
-				"Internal server error"
-			);
-		}
-	}
+    public static async createNewUser(user: IUser): Promise<IUser> {
+        try {
+            const response = await this.Model.create({
+                username: user.username,
+                email: user.email,
+                password: user.password,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                createdAt: Date.now(),
+                updatedAt: Date.now(),
+            });
+            const createdUser: IUser = {
+                _id: response._id,
+                username: response.username,
+                email: response.email,
+                password: response.password,
+                firstName: response.firstName,
+                lastName: response.lastName,
+                createdAt: response.createdAt,
+                updatedAt: response.updatedAt,
+            };
+            return createdUser;
+        } catch (error) {
+            console.error(this.name, "createNewUser error: ", error);
+            throw ErrorHandler.createError(
+                "InternalServerError",
+                "Internal server error"
+            );
+        }
+    }
+    public static async getAllUsers() {
+        try {
+            const response = await this.Model.find();
+            const allUsers: IUser[] = [];
+            response.forEach((user) => {
+                allUsers.push({
+                    _id: user._id,
+                    username: user.username,
+                    email: user.email,
+                    password: user.password,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    createdAt: user.createdAt,
+                    updatedAt: user.updatedAt,
+                });
+            });
+            return allUsers;
+        } catch (error) {
+            console.error(this.name, "getAllUsers error", error);
+            throw ErrorHandler.createError(
+                "InternalServerError",
+                "Internal server error"
+            );
+        }
+    }
 
-	// public static async findAllLists(): Promise<IList[] | null> {
-	// 	try {
-	// 		const lists = await this.Model.find().exec();
-	// 		return lists || null;
-	// 	} catch (error) {
-	// 		console.error(this.name, "getAllLists error:", error);
-	// 		throw error;
-	// 	}
-	// }
+    public static async updateUser(
+        userId: string,
+        user: IUser
+    ): Promise<IUser> {
+        try {
+            const userData = {
+                username: user.username,
+                email: user.email,
+                password: user.password,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                updatedAt: Date.now(),
+            };
+
+            const response = await this.Model.findByIdAndUpdate(
+                userId,
+                userData,
+                {
+                    new: true,
+                }
+            );
+
+            if (response) {
+                const userUpdated: IUser = {
+                    _id: response._id,
+                    username: response.username,
+                    email: response.email,
+                    password: response.password,
+                    firstName: response.firstName,
+                    lastName: response.lastName,
+                    updatedAt: response.updatedAt,
+                };
+                return userUpdated;
+            }
+            throw ErrorHandler.createError(
+                "InternalServerError",
+                "User was not updated"
+            );
+        } catch (error) {
+            console.error(this.name, "updateUser error: ", error);
+            throw ErrorHandler.createError(
+                "InternalServerError",
+                "Internal server error"
+            );
+        }
+    }
+
+    public static async deleteUser(userId: string): Promise<IUser | null> {
+        try {
+            const response = await this.Model.findByIdAndDelete(userId);
+
+            if (response === null) return null;
+
+            const user: IUser = {
+                _id: response._id,
+                username: response.username,
+                email: response.email,
+                password: response.password,
+                firstName: response.firstName,
+                lastName: response.lastName,
+            };
+            return user;
+        } catch (error) {
+            console.error(this.name, "deleteUser error: ", error);
+            throw ErrorHandler.createError(
+                "InternalServerError",
+                "Internal server error"
+            );
+        }
+    }
+
+    // public static async findAllLists(): Promise<IList[] | null> {
+    // 	try {
+    // 		const lists = await this.Model.find().exec();
+    // 		return lists || null;
+    // 	} catch (error) {
+    // 		console.error(this.name, "getAllLists error:", error);
+    // 		throw error;
+    // 	}
+    // }
 }
