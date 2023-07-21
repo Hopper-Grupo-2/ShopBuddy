8fa3226c76a237ee61de5fc3f60726624c6bfbb0
Felipe Fernandes <felipe.bravil@gmail.com>
2023-07-20 22:30:57 -0300

    feat: rework integration process

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,16 @@ WORKDIR /app
 COPY package*.json ./
 RUN npm install
 
+# Change to client directory and install dependencies there
+WORKDIR /app/client
+COPY client/package*.json ./
+RUN npm install
+
+# Switch back to /app as working directory
+WORKDIR /app
+
 COPY . .
 
-
-EXPOSE 4021
+#EXPOSE 4021
 
 #CMD ["npm", "run", "dev"]
--- server/src/routers/users.ts
+++ server/src/routers/users.ts
@@ -8,7 +8,7 @@ import handleValidation from "../validators/handle-validation";
 const usersRouter = Router();
 
 // GET /api/users/me - get the authenticated user
-usersRouter.get("/me", UsersController.getMe);
+usersRouter.get("/me", authenticate, UsersController.getMe);
 
 // GET /api/users/ - get all users (not useful)
 usersRouter.get("/", UsersController.getAllUsers);
