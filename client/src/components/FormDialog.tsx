d6dfa81421c9f64f06d71f0530bb10c573b4709f
Letonio <letoniojs@gmail.com>
2023-07-24 22:40:56 -0300

    feat: connect get members with frontend

--- client/src/components/FormDialog.tsx
+++ client/src/components/FormDialog.tsx
@@ -1,78 +1,78 @@
 import React, { useState } from "react";
 import {
-	Button,
-	Dialog,
-	DialogActions,
-	DialogContent,
-	DialogTitle,
-	TextField,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    TextField,
 } from "@mui/material";
 
 interface FormField {
-	id: string;
-	label: string;
-	type: string;
+    id: string;
+    label: string;
+    type: string;
 }
 
 interface FormDialogProps {
-	title: string;
-	fields: FormField[]; //form fields
-	open: boolean;
-	handleClose: () => void;
-	handleSubmit: (formData: Record<string, string>) => Promise<boolean>;
+    title: string;
+    fields: FormField[]; //form fields
+    open: boolean;
+    handleClose: () => void;
+    handleSubmit: (formData: Record<string, string>) => Promise<boolean>;
 }
 
 const FormDialog: React.FC<FormDialogProps> = (props: FormDialogProps) => {
-	const initialFormData: Record<string, string> = props.fields.reduce(
-		(acc, field) => ({ ...acc, [field.id]: "" }),
-		{}
-	);
-	const [formData, setFormData] =
-		useState<Record<string, string>>(initialFormData);
+    const initialFormData: Record<string, string> = props.fields.reduce(
+        (acc, field) => ({ ...acc, [field.id]: "" }),
+        {}
+    );
+    const [formData, setFormData] =
+        useState<Record<string, string>>(initialFormData);
 
-	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		setFormData((prevFormData) => ({
-			...prevFormData,
-			[event.target.id]: event.target.value,
-		}));
-	};
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData((prevFormData) => ({
+            ...prevFormData,
+            [event.target.id]: event.target.value,
+        }));
+    };
 
-	const handleSubmit = async (event: React.FormEvent) => {
-		event.preventDefault();
-		const success = await props.handleSubmit(formData);
-		if (success) {
-			setFormData(initialFormData);
-			props.handleClose();
-		}
-	};
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+        const success = await props.handleSubmit(formData);
+        if (success) {
+            setFormData(initialFormData);
+            props.handleClose();
+        }
+    };
 
-	return (
-		<Dialog open={props.open} onClose={props.handleClose}>
-			<DialogTitle>{props.title}</DialogTitle>
-			<form onSubmit={handleSubmit}>
-				<DialogContent>
-					{props.fields.map((field) => (
-						<TextField
-							autoFocus
-							margin="dense"
-							key={field.id}
-							id={field.id}
-							label={field.label}
-							type={field.type}
-							fullWidth
-							variant="standard"
-							value={formData[field.id] || ""}
-							onChange={handleChange}
-						/>
-					))}
-				</DialogContent>
-				<DialogActions>
-					<Button onClick={props.handleClose}>Cancelar</Button>
-					<Button type="submit">Confirmar</Button>
-				</DialogActions>
-			</form>
-		</Dialog>
-	);
+    return (
+        <Dialog open={props.open} onClose={props.handleClose}>
+            <DialogTitle>{props.title}</DialogTitle>
+            <form onSubmit={handleSubmit}>
+                <DialogContent>
+                    {props.fields.map((field) => (
+                        <TextField
+                            autoFocus
+                            margin="dense"
+                            key={field.id}
+                            id={field.id}
+                            label={field.label}
+                            type={field.type}
+                            fullWidth
+                            variant="standard"
+                            value={formData[field.id] || ""}
+                            onChange={handleChange}
+                        />
+                    ))}
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={props.handleClose}>Cancelar</Button>
+                    <Button type="submit">Confirmar</Button>
+                </DialogActions>
+            </form>
+        </Dialog>
+    );
 };
 
 export { FormDialog };
--- client/src/views/list/ListPage.tsx
+++ client/src/views/list/ListPage.tsx
@@ -9,37 +9,39 @@ import IList from "../../interfaces/iList";
 import { FormDialog } from "../../components/FormDialog";
 import ChatBox from "../../components/ChatBox";
 import styled from "@emotion/styled";
+import { MembersModal } from "../../components/MembersModal";
+import IUser from "../../interfaces/iUser";
 
 const ContentContainer = styled.div`
-	display: flex;
-	flex-direction: row;
-	justify-content: space-between;
-	flex-wrap: wrap;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    flex-wrap: wrap;
 
-	@media (max-width: 350px) {
-		flex-direction: column;
-	}
+    @media (max-width: 350px) {
+        flex-direction: column;
+    }
 `;
 
 const HeaderContainer = styled.div`
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	flex-wrap: wrap;
-	padding: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+    padding: 10px;
 
-	@media (max-width: 600px) {
-		flex-direction: column;
-	}
+    @media (max-width: 600px) {
+        flex-direction: column;
+    }
 `;
 
 const ButtonContainer = styled.div`
-	display: flex;
-	gap: 10px;
+    display: flex;
+    gap: 10px;
 
-	@media (max-width: 600px) {
-		margin-bottom: 10px;
-	}
+    @media (max-width: 600px) {
+        margin-bottom: 10px;
+    }
 `;
 
 // import "./ListPage.css";
@@ -72,163 +74,206 @@ const ButtonContainer = styled.div`
 ]; */
 
 export default function List() {
-	const params = useParams();
-	const [list, setList] = useState<IList>();
-	const [items, setItems] = useState<Array<IItem>>([]);
-	const [openItemForm, setOpenItemForm] = useState(false);
+    const params = useParams();
+    const [list, setList] = useState<IList>();
+    const [items, setItems] = useState<Array<IItem>>([]);
+    const [openItemForm, setOpenItemForm] = useState(false);
 
-	useEffect(() => {
-		const fetchList = async () => {
-			const response = await fetch(`/api/lists/${params.listId}`, {
-				method: "GET",
-				credentials: "include", // Ensure credentials are sent
-			});
+    // create use state to save members
+    const [members, setMembers] = useState<Array<IUser>>([]);
+    // members
+    const [showMembers, setShowMembers] = useState(false);
 
-			if (response.ok) {
-				const listData = await response.json();
-				console.log(listData);
-				setList(listData.data);
-				setItems(listData.data.products);
-			}
-		};
+    useEffect(() => {
+        const fetchList = async () => {
+            const response = await fetch(`/api/lists/${params.listId}`, {
+                method: "GET",
+                credentials: "include", // Ensure credentials are sent
+            });
 
-		fetchList();
-	}, []);
+            if (response.ok) {
+                const listData = await response.json();
+                console.log(listData);
+                setList(listData.data);
+                setItems(listData.data.products);
+            }
+        };
 
-	const handleOpenItemForm = () => {
-		setOpenItemForm(true);
-	};
-	const handleCloseListForm = () => {
-		setOpenItemForm(false);
-	};
+        fetchList();
 
-	const createNewItem = async (formData: Record<string, string>) => {
-		try {
-			const response = await fetch(
-				`/api/lists/${params.listId}/products`,
-				{
-					method: "PATCH",
-					headers: {
-						"Content-Type": "application/json",
-					},
-					body: JSON.stringify({
-						name: formData["name"],
-						quantity: formData["quantity"],
-						unit: formData["unit"],
-						price: formData["price"],
-						checked: false,
-					}),
-				}
-			);
+        // fazer um fetch que pega os membros baseado no id
 
-			const responseObj = await response.json();
-			if (response.ok) {
-				const products = responseObj.data.products;
-				setItems(products);
-				//alert("Item adicionado com sucesso!");
-				console.log(responseObj);
-				//setFetchTrigger(!fetchTrigger);
-				return true;
-			} else {
-				throw responseObj.error;
-			}
-		} catch (error: any) {
-			console.error(error.name, error.message);
-			alert("Failed to add item: " + error.message);
-			return false;
-		}
-	};
+        const fetchMembers = async () => {
+            console.log("esse é o list id:", params.listId);
+            const response = await fetch(
+                `/api/lists/${params.listId}/members`,
+                {
+                    method: "GET",
+                    credentials: "include", // Ensure credentials are sent
+                }
+            );
 
-	// this function has to return a boolean for handling the button state later
-	// trust me we: will need this
-	const handleDeleteProduct = async (productId: string) => {
-		if (!confirm(`Do you want to remove the product ${productId}?`)) return;
-		try {
-			const response = await fetch(
-				`/api/lists/${list?._id}/products/${productId}`,
-				{
-					method: "DELETE",
-					headers: {
-						"Content-Type": "application/json",
-					},
-				}
-			);
+            if (response.ok) {
+                const membersData = await response.json();
+                console.log("members:", membersData);
+                setMembers(membersData.data as IUser[]);
+            }
+        };
 
-			const responseObj = await response.json();
-			if (response.ok) {
-				removeItem(productId);
-				return true;
-			} else {
-				throw responseObj.error;
-			}
-		} catch (error: any) {
-			console.error(error.name, error.message);
-			alert("Failed to remove item: " + error.message);
-			return false;
-		}
-	};
+        fetchMembers();
+    }, []);
 
-	function checkItem(itemId: string) {
-		const currentItem = items.filter((item) => item._id === itemId)[0];
-		const currentIndex = items.indexOf(currentItem);
-		items[currentIndex].checked = !items[currentIndex].checked;
-		setItems([...items]);
-	}
+    const handleOpenItemForm = () => {
+        setOpenItemForm(true);
+    };
+    const handleCloseListForm = () => {
+        setOpenItemForm(false);
+    };
 
-	function removeItem(itemId: string) {
-		const filteredItems = items.filter((item) => item._id !== itemId);
-		setItems([...filteredItems]);
-	}
+    //show/hide members
+    const handleShowMembers = () => {
+        setShowMembers(true);
+    };
 
-	return (
-		<>
-			<PageStructure>
-				<HeaderContainer>
-					<h1>{list?.listName}</h1>
-					<ButtonContainer>
-						<Button variant="contained">Members</Button>
-						<Button variant="contained">+ Add member</Button>
-					</ButtonContainer>
-				</HeaderContainer>
-				<ContentContainer>
-					<SimplePaper>
-						{list?.products.length === 0 || !list ? (
-							<p style={{ textAlign: "center" }}>
-								A lista está vazia...
-							</p>
-						) : (
-							<CheckboxList
-								items={items}
-								onCheck={checkItem}
-								onRemove={handleDeleteProduct}
-							/>
-						)}
-						<Button
-							sx={{
-								margin: "0px auto 15px auto",
-								display: "block",
-							}}
-							variant="contained"
-							onClick={handleOpenItemForm}
-						>
-							Adicionar item
-						</Button>
-					</SimplePaper>
-					<ChatBox />
-				</ContentContainer>
-			</PageStructure>
-			<FormDialog
-				title="Adicionar novo item"
-				fields={[
-					{ id: "name", label: "Nome do item", type: "text" },
-					{ id: "unit", label: "Unidade de medida", type: "text" }, //will be a select
-					{ id: "quantity", label: "Quantidade", type: "text" },
-					{ id: "price", label: "Preço/unidade", type: "text" },
-				]}
-				open={openItemForm}
-				handleClose={handleCloseListForm}
-				handleSubmit={createNewItem}
-			/>
-		</>
-	);
+    const handleHideMembers = () => {
+        setShowMembers(false);
+    };
+
+    const createNewItem = async (formData: Record<string, string>) => {
+        try {
+            const response = await fetch(
+                `/api/lists/${params.listId}/products`,
+                {
+                    method: "PATCH",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        name: formData["name"],
+                        quantity: formData["quantity"],
+                        unit: formData["unit"],
+                        price: formData["price"],
+                        checked: false,
+                    }),
+                }
+            );
+
+            const responseObj = await response.json();
+            if (response.ok) {
+                const products = responseObj.data.products;
+                setItems(products);
+                //alert("Item adicionado com sucesso!");
+                console.log(responseObj);
+                //setFetchTrigger(!fetchTrigger);
+                return true;
+            } else {
+                throw responseObj.error;
+            }
+        } catch (error: any) {
+            console.error(error.name, error.message);
+            alert("Failed to add item: " + error.message);
+            return false;
+        }
+    };
+
+    // this function has to return a boolean for handling the button state later
+    // trust me we: will need this
+    const handleDeleteProduct = async (productId: string) => {
+        if (!confirm(`Do you want to remove the product ${productId}?`)) return;
+        try {
+            const response = await fetch(
+                `/api/lists/${list?._id}/products/${productId}`,
+                {
+                    method: "DELETE",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            );
+
+            const responseObj = await response.json();
+            if (response.ok) {
+                removeItem(productId);
+                return true;
+            } else {
+                throw responseObj.error;
+            }
+        } catch (error: any) {
+            console.error(error.name, error.message);
+            alert("Failed to remove item: " + error.message);
+            return false;
+        }
+    };
+
+    function checkItem(itemId: string) {
+        const currentItem = items.filter((item) => item._id === itemId)[0];
+        const currentIndex = items.indexOf(currentItem);
+        items[currentIndex].checked = !items[currentIndex].checked;
+        setItems([...items]);
+    }
+
+    function removeItem(itemId: string) {
+        const filteredItems = items.filter((item) => item._id !== itemId);
+        setItems([...filteredItems]);
+    }
+
+    return (
+        <>
+            <PageStructure>
+                <HeaderContainer>
+                    <h1>{list?.listName}</h1>
+                    <ButtonContainer>
+                        <Button variant="contained" onClick={handleShowMembers}>
+                            Members
+                        </Button>
+                        <Button variant="contained">+ Add member</Button>
+                    </ButtonContainer>
+                </HeaderContainer>
+                <ContentContainer>
+                    <SimplePaper>
+                        {list?.products.length === 0 || !list ? (
+                            <p style={{ textAlign: "center" }}>
+                                A lista está vazia...
+                            </p>
+                        ) : (
+                            <CheckboxList
+                                items={items}
+                                onCheck={checkItem}
+                                onRemove={handleDeleteProduct}
+                            />
+                        )}
+                        <Button
+                            sx={{
+                                margin: "0px auto 15px auto",
+                                display: "block",
+                            }}
+                            variant="contained"
+                            onClick={handleOpenItemForm}
+                        >
+                            Adicionar item
+                        </Button>
+                    </SimplePaper>
+                    <ChatBox />
+                </ContentContainer>
+            </PageStructure>
+            <FormDialog
+                title="Adicionar novo item"
+                fields={[
+                    { id: "name", label: "Nome do item", type: "text" },
+                    { id: "unit", label: "Unidade de medida", type: "text" }, //will be a select
+                    { id: "quantity", label: "Quantidade", type: "text" },
+                    { id: "price", label: "Preço/unidade", type: "text" },
+                ]}
+                open={openItemForm}
+                handleClose={handleCloseListForm}
+                handleSubmit={createNewItem}
+            />
+            <MembersModal
+                title="Todos os membros da lista"
+                members={members}
+                open={showMembers}
+                handleClose={handleHideMembers}
+            />
+        </>
+    );
 }
