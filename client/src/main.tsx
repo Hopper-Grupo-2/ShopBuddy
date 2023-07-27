6800a9184c01e8a2b49c6ace8013a4ef3dda8915
Felipe Fernandes <felipe.bravil@gmail.com>
2023-07-27 09:09:44 -0300

    feat: provide socket upon authentication

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { UserProvider } from "./contexts/UserProvider.tsx";
-import { SocketProvider } from "./contexts/SocketProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<UserProvider>
-			<SocketProvider>
-				<App />
-			</SocketProvider>
+			<App />
 		</UserProvider>
 	</React.StrictMode>
 );
