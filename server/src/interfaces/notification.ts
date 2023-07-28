af1954742229e7990c3b8d2a3313fdb664ced40a
Felipe Fernandes <felipe.bravil@gmail.com>
2023-07-28 17:51:36 -0300

    feat: notification entity created

---
+++ server/src/interfaces/notification.ts
@@ -0,0 +1,18 @@
+import { Schema } from "mongoose";
+
+export default interface INotification {
+	_id: Schema.Types.ObjectId;
+	listId: Schema.Types.ObjectId | string;
+	userId: Schema.Types.ObjectId | string;
+	type: NotificationTypes;
+	textContent: string;
+	read: boolean;
+	createdAt: Date;
+	updatedAt: Date;
+}
+
+export enum NotificationTypes {
+	ADDED_TO_LIST = "ADDED_TO_LIST",
+	REMOVED_FROM_LIST = "REMOVED_FROM_LIST",
+	MESSAGE_FROM_LIST = "MESSAGE_FROM_LIST",
+}
