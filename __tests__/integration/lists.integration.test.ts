24556726ea5c83ab554f292a29453cfd32a7c632
joaovsilva18 <joaovactorsilva@gmail.com>
2023-07-31 18:08:49 -0300

    add unit-test-integration getListByListId

--- __tests__/integration/lists.integration.test.ts
+++ __tests__/integration/lists.integration.test.ts
@@ -49,4 +49,25 @@ describe("GET /api/lists:listid", () => {
       expect(response.body.data).toMatchObject(createdList);
      
   });
-});
+  it("should return status 500 if the provided list ID is invalid", async () => {
+    const userInfo: IUser = {
+        username: "joaoteste12",
+        password: "123",
+        email: "joaots@alpha.com",
+        firstName: "joao",
+        lastName: "test",
+      };
+      const userExample: IUser = await UsersRepositories.createNewUser(userInfo);
+      const listName = "Listinha";
+      const jtwCookie = generateCookieForUser(userExample);
+      const userid = userExample._id?.toString() || "";
+
+    const createdList = await ListRepositories.createNewList(listName, userid);
+    const invalidListId = "invalid-id";
+
+    const response = await request
+      .get(`/api/lists/${invalidListId}`)
+      .set("Cookie", `session=${jtwCookie}`);
+
+    expect(response.status).toBe(500);
+})});
