39faa1319a02280d5fe5ec1bcc76cb6b451fd9a1
joaovsilva18 <joaovactorsilva@gmail.com>
2023-07-31 12:14:12 -0300

    add test-unit-repo findAllListsByUserId

--- __tests__/unit/repositories/listsRepositories.test.ts
+++ __tests__/unit/repositories/listsRepositories.test.ts
@@ -68,4 +68,74 @@ import {
       });
     });
   });
-  
+
+    describe("findAllListsByUserId", () => {
+    it("should retrieve all lists associated with the user", async () => {
+      const Model = Models.getInstance().listModel;
+
+      // Create a user and two lists associated with the user
+      const user = await Models.getInstance().userModel.create({
+        username: "testuser1",
+        email: "testuser1@alpha.com",
+        password: "123",
+        firstName: "test",
+        lastName: "user1",
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+      });
+
+      const list1 = await Model.create({
+        listName: "lista1",
+        owner: user._id, // Set the owner to the user's ID
+        members: [{ userId: user._id }], // Add the user as a member
+        // Add other list properties as needed
+      });
+
+      const list2 = await Model.create({
+        listName: "lista2",
+        owner: user._id, // Set the owner to the user's ID
+        members: [{ userId: user._id }], // Add the user as a member
+        // Add other list properties as needed
+      });
+
+      // Call the method to get all lists associated with the user
+      const allLists = await ListRepositories.findAllListsByUserId(user.id);
+
+      // Assert that the result contains the inserted lists
+      expect(allLists).toHaveLength(2);
+      if (allLists) {
+        expect(allLists[0].listName).toBe("lista1");
+        expect(allLists[1].listName).toBe("lista2");
+      }
+    });
+
+    it("should return an empty array if no lists are associated with the user", async () => {
+      // Create a user, but no lists associated with the user
+      const user = await Models.getInstance().userModel.create({
+        username: "testuser1",
+        email: "testuser1@alpha.com",
+        password: "123",
+        firstName: "test",
+        lastName: "user1",
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+      });
+
+      // Call the function being tested
+      const lists = await ListRepositories.findAllListsByUserId(user.id);
+
+      // Assert the expected results
+      expect(lists).toEqual([]);
+    });
+
+    it("should return an empty array if the user ID does not exist", async () => {
+      // Call the function being tested with a non-existent user ID
+      const nonExistentUserId = "1a3f5b2e9c7d0f8e6b4a9c8e";
+      const lists = await ListRepositories.findAllListsByUserId(
+        nonExistentUserId
+      );
+
+      // Assert the expected results
+      expect(lists).toEqual([]);
+    });
+  });
