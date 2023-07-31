4c5042dedede14295c465b6c4738af7aeecc3998
joaovsilva18 <joaovactorsilva@gmail.com>
2023-07-31 11:59:52 -0300

    add test-unit-repo getAllLists

---
+++ __tests__/unit/repositories/listsRepositories.test.ts
@@ -0,0 +1,71 @@
+import {
+    connectDatabase,
+    closeDatabase,
+    clearDatabase,
+  } from "../../helpers/db";
+  import Models from "../../../server/src/database/models";
+  import ListRepositories from "../../../server/src/repositories/lists";
+  
+  beforeAll(async () => {
+    await connectDatabase();
+  });
+  
+  afterEach(async () => {
+    await clearDatabase();
+  });
+  
+  afterAll(async () => {
+    await closeDatabase();
+  });
+  
+  describe("Lists Repositories", () => {
+    describe("getAllLists", () => {
+      it("should retrieve all lists from the database", async () => {
+         // Criar um usuário para ser o owner das listas
+      const userModel = Models.getInstance().userModel;
+      const user = await userModel.create({
+        username: "testuser1",
+        email: "testuser1@alpha.com",
+        password: "123",
+        firstName: "test",
+        lastName: "user1",
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+      });
+        const Model = Models.getInstance().listModel;
+  
+        // insert some lists into database
+        const list1 = await Model.create({
+            listName: "lista1",
+            owner: user._id, // Definir o owner como o ID do usuário criado
+          });
+    
+          const list2 = await Model.create({
+            listName: "lista2",
+            owner: user._id, // Definir o owner como o ID do usuário criado
+          });
+    
+  
+        // call the method to get all lists from the repository
+        const allLists = await ListRepositories.findAllLists();
+  
+        // assert that the result contains the inserted lists
+        expect(allLists).toHaveLength(2);
+  
+        // Check if the array is not null before accessing its elements
+        if (allLists) {
+          expect(allLists[0].listName).toBe("lista1");
+          expect(allLists[1].listName).toBe("lista2");
+        }
+      });
+  
+      it("should return an empty array if no lists exist", async () => {
+        // call the function being tested
+        const lists = await ListRepositories.findAllLists();
+  
+        // assert the expected results
+        expect(lists).toEqual([]);
+      });
+    });
+  });
+  
