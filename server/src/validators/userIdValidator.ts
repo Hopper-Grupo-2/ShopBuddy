f098a31dc940c47ec087d79e8d23e1da7c57f747
Letonio <letoniojs@gmail.com>
2023-07-31 21:29:06 -0300

    refactor: remove files unused

--- server/src/validators/userIdValidator.ts
+++
@@ -1,9 +0,0 @@
-import { check } from "express-validator";
-
-export default function userIdValidator() {
-    return [
-        check("userId")
-            .isMongoId()
-            .withMessage("The id must be a valid ObjectId."),
-    ];
-}
