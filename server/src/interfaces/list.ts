67b3e100ebbcd1151834165ae5e8d11f03af926f
Davi Severino <daviseverino96@gmail.com>
2023-08-01 01:06:00 -0300

    test: Add messages services

--- server/src/interfaces/list.ts
+++ server/src/interfaces/list.ts
@@ -3,16 +3,14 @@ import { Schema } from "mongoose";
 export default interface IList {
   _id: Schema.Types.ObjectId | string;
   listName: string;
-  products: [
-    {
+  products: {
       name: string;
       quantity: Number;
       unit: string;
       price: Number;
       checked: Boolean;
       _id: Schema.Types.ObjectId | string;
-    }
-  ];
+    }[];
   owner: Schema.Types.ObjectId | string;
   members: IMember[];
   createdAt: Date;
--- server/src/interfaces/message.ts
+++ server/src/interfaces/message.ts
@@ -1,7 +1,7 @@
 import { Schema } from "mongoose";
 
 export default interface IMessage {
-	_id: Schema.Types.ObjectId;
+	_id: Schema.Types.ObjectId | string;
 	listId: Schema.Types.ObjectId | string;
 	userId: Schema.Types.ObjectId | string;
 	textContent: string;
