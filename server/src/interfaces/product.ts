c5bca5f129a4b7141dd9c969b1d79fdb7702de97
Felipe Fernandes <felipe.bravil@gmail.com>
2023-08-04 13:44:44 -0300

    feat: forgot in previous commit

--- server/src/interfaces/product.ts
+++ server/src/interfaces/product.ts
@@ -1,11 +1,11 @@
 import { Schema } from "mongoose";
 
 export default interface IProduct {
+  _id?: Schema.Types.ObjectId | string;
   name: string;
   quantity: Number;
   unit: string;
   price?: Number;
   market?: String;
   checked?: Boolean;
-  _id?: Schema.Types.ObjectId | string;
 }
