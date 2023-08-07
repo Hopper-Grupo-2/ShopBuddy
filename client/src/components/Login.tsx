382b0bbfcb738b3c1788019d0a047faa113e5129
joaovsilva18 <joaovactorsilva@gmail.com>
2023-08-07 20:53:29 -0300

    style: Login page

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -98,47 +98,80 @@ export default function LogIn() {
               flexDirection: "column",
               alignItems: "center",
             }}
-          >
+            >
             <Typography
               variant="h5"
               sx={{
                 marginTop: { xs: "3vh", md: 0 },
-                marginBottom: "3vh", // Adiciona um espaçamento inferior
-                fontSize: { xs: "36px", md: "43px" }, // Tamanho de fonte responsivo
+                marginBottom: "10vh",
+                fontSize: {xs: "6vw", md: "4vw" },
                 fontWeight: "bold",
                 fontFamily: "'Open Sans'",
+                whiteSpace: "nowrap"
               }}
             >
               Seja Bem vindo!
             </Typography>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Nome de Usuário"
-              name="email"
-              autoComplete="email"
-              autoFocus
+            <Box
+              component="div"
               sx={{
-                width: "400px",
-                border: "1px solid black",
+                width: "90%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
               }}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Senha"
-              type="password"
-              id="password"
-              autoComplete="current-password"
+            >
+              <Avatar
+                src="./src/assets/icons/email-icon.svg"
+                alt="Ícone"
+                sx={{ width: 24, height: 24,marginRight: "0.5vw" }}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Nome de Usuário"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                sx={{
+                  width: "93%",
+                  border: "2px solid #000000",
+                  marginBottom: "2vh"
+                }}
+              />
+            </Box>
+            <Box
+              component="div"
               sx={{
-                width: "400px",
-                border: "1px solid black",
+                width: "90%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
               }}
-            />
+            >
+              <Avatar
+                src="./src/assets/icons/lock-icon.svg"
+                alt="Ícone"
+                sx={{ width: 24, height: 24, marginRight: "0.5vw"}}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Senha"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                sx={{
+                  width: "93%",
+                  border: "2px solid #000000",
+                  marginBottom: "2vh"
+                }}
+              />
+            </Box>
             <Grid container justifyContent="center">
               <Grid item>
                 <Typography variant="body1">
@@ -155,9 +188,15 @@ export default function LogIn() {
               variant="contained"
               sx={{
                 background: "orange",
+                display: "flex",
+                alignItems: "center",
                 marginTop: "3vh",
                 marginBottom: "2vh",
+                fontSize: "1.3rem",
+                fontWeight: "600",
                 width: "350px",
+                height: "4rem",
+                padding: "1rem"
               }}
             >
               entrar
