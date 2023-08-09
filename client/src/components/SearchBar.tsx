dbeed4be02a9e89b0d76a67fff0418cf5df1e2c6
Felipe Fernandes <felipe.bravil@gmail.com>
2023-08-09 17:36:43 -0300

    feat: add search bar component

---
+++ client/src/components/SearchBar.tsx
@@ -0,0 +1,34 @@
+import { InputAdornment, TextField } from "@mui/material";
+import { useState } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+
+interface SearchBarProps {
+  onChange: (term: string) => void;
+}
+
+export default function SearchBar(props: SearchBarProps) {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+    props.onChange(event.target.value);
+  };
+
+  return (
+    <TextField
+      id="search"
+      type="search"
+      label="Search"
+      value={searchTerm}
+      onChange={handleChange}
+      sx={{ minWidth: 350 }}
+      InputProps={{
+        endAdornment: (
+          <InputAdornment position="end">
+            <SearchIcon />
+          </InputAdornment>
+        ),
+      }}
+    />
+  );
+}
