f7d604a934ed800e3b5134dc93f1df7690e0654b
Felipe Fernandes <felipe.bravil@gmail.com>
2023-08-20 22:56:14 -0300

    feat: search feedback (and minor fixes)

--- client/src/components/ListCard.tsx
+++ client/src/components/ListCard.tsx
@@ -31,7 +31,6 @@ export default function ListCard(props: CardProps) {
     year: "numeric",
     hour: "2-digit",
     minute: "2-digit",
-    second: "2-digit",
   }).format(props.date);
 
   return (
