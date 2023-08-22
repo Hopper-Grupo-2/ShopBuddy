fa5a6622454e854b9452a74519510cfca09abe6d
Letonio <letoniojs@gmail.com>
2023-08-21 23:59:52 -0300

    fix: change path from fallback method

--- server/src/app.ts
+++ server/src/app.ts
@@ -61,7 +61,9 @@ export default class App {
 
   private fallback(): void {
     this.app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "../../client/dist", "index.html"));
+      res.sendFile(
+        path.resolve(__dirname, "../../../client/dist", "index.html")
+      );
     });
   }
 
